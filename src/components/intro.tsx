dadfbd67e51bfd1f4b5178b097bf0b74c84d9e29
MARCOS <marcossuarezr88@gmail.com>
2024-04-04 21:38:23 +0200

    tsconfig gooD

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -32,7 +32,7 @@ export default function Intro() {
           >
             <Image
               src="https://res.cloudinary.com/dxrttyi2g/image/upload/v1702408502/marcos_enoqcl.png"
-              alt="Ricardo portrait"
+              alt="Marcos portrait"
               width="192"
               height="192"
               quality="95"
@@ -62,7 +62,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Marcos.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
