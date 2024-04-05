833bb592f6d101eaf74f50a3d5f8436a6c30a001
MARCOS <marcossuarezr88@gmail.com>
2024-04-05 09:21:15 +0200

    tsconfig gooD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
-
-import { Inter } from "next/font/google";
+import Header from "@/components/header";
 import "./globals.css";
-
-import Header from "@/components/header"
+import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Marcos | Personal Portfolio",
   description: "Marcos is a full-stack developer with 2 years of experience.",
 };
+
 
 export default function RootLayout({
   children,
